cb162eda6ec7cdcd58a0967183a67e575fa9417a
Yura Lim <yura6068@gmail.com>
2019-07-07 08:11:35 +0000

    refactor: Use @hourly for the readability

--- airflow/dags/udac_example_dag.py
+++ airflow/dags/udac_example_dag.py
@@ -22,7 +22,7 @@ default_args = {
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 0 * * *')
+          schedule_interval='@hourly')
 
 start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
 
